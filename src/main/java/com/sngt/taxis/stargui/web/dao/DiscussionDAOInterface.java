01087d04f3b8c6bf497398a8a0f985a570e55d5b
EstebanRico <kyphans@gmail.com>
2015-10-06 11:17:55 +0200

    Test de partage d'une bdd commune

---
+++ src/main/java/com/sngt/taxis/stargui/web/dao/DiscussionDAOInterface.java
@@ -0,0 +1,15 @@
+package com.sngt.taxis.stargui.web.dao;
+
+import com.sngt.taxis.stargui.web.model.Discussion;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by skiss on 14/09/2015.
+ */
+public interface DiscussionDAOInterface extends CrudRepository<Discussion, Integer> {
+
+
+}
+
